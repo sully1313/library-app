daf7117ac4f3a3578632d782c89fd14290b18322
Matt Sullivan <mattbsullivan@gmail.com>
2016-06-19 13:47:54 -0700

    first commit

---
+++ lib_app/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+root to: "users#index"
+
+get "/users", to: "users#index", as: "users"
+get "/users/new", to: "users#new", as: "new_user"
+post "/users", to: "users#create"
+get "/users/:id", to: "users#show"
+
+#sign_in
+get "/sign_in", to: "sessions#new"
+post "/sessions", to: "sessions#create"
+
+end
